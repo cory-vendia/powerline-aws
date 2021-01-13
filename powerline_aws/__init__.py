693fcb9f45919d28b35473eaf660a56756ecbbe4
Cory Forsythe <75388221+cory-vendia@users.noreply.github.com>
2021-01-13 09:55:11 -0600

    use AWSUME PROFILE var

--- powerline_aws/__init__.py
+++ powerline_aws/__init__.py
@@ -4,6 +4,6 @@ from powerline.theme import requires_segment_info
 
 @requires_segment_info
 def aws_profile(pl: PowerlineLogger, segment_info: dict) -> str:
-    profile = str(segment_info['environ'].get('AWS_PROFILE', '@'))
+    profile = str(segment_info['environ'].get('AWSUME_PROFILE', '@'))
     (role, account) = profile.split('@')
     return account or '@'
